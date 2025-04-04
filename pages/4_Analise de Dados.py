286773c32963953563eece51a4fb42b7a67f30b6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-04 07:38:21 -0300

    teste

--- pages/4_Analise de Dados.py
+++ pages/4_Analise de Dados.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from scipy.stats import norm, poisson, binom, expon
+from scipy.stats import poisson, binom, expon
 
 # Configuração da página
 st.set_page_config(
